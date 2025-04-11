4b1f690c084decf9e7f3e03c5562d90caa2d25dc
LeskhyAnneHDumangas <163563678+LeskhyAnneHDumangas@users.noreply.github.com>
2025-04-11 10:16:40 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,10 +3,9 @@ from app import calculate_circumference_of_circle
 import math
 
 
-def test_calculate_area_of_circle(): 
+def test_calculate_area_of_circle():
     assert calculate_area_of_circle(5) == math.pi * 25
 
 
-def test_calculate_circumference_of_circle(): 
+def test_calculate_circumference_of_circle():
     assert calculate_circumference_of_circle(5) == 2 * math.pi * 5
-    
