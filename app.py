64130cd033a0bd2e38f201d935eb156e1e5eef81
LeskhyAnneHDumangas <leskhyanne@gmail.com>
2025-04-11 10:11:35 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+def calculate_area_of_circle(radius): 
+    import math 
+    return math.pi * radius**2 
+
+def calculate_circumference_of_circle(radius):
+    import math 
+    return 2 * math.pi * radius
---
+++ test_app.py
@@ -0,0 +1,9 @@
+from app import calculate_area_of_circle
+from app import calculate_circumference_of_circle
+import math
+
+def test_calculate_area_of_circle(): 
+    assert calculate_area_of_circle(5) == math.pi * 25
+    
+def test_calculate_circumference_of_circle(): 
+    assert calculate_circumference_of_circle(5) == 2 * math.pi * 5
